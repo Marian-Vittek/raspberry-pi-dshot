eeb980c650a08e7e1af31f5a7268664eb5956655
Marian Vittek <vittek@xrefactory.com>
2023-05-09 14:49:48 +0200

    bidirectional spin

--- motor-dshot.c
+++ motor-dshot.c
@@ -25,16 +25,17 @@ https://stackoverflow.com/questions/69425540/execute-mmap-on-linux-kernel
 // Select the dshot version you want to use. Value may be 150, 300,
 // 600 or 1200. DSHOT_VERSION 150 seems to work fine on raspberry pi
 // zero 2, other values may not work.
+#ifndef DSHOT_VERSION
 #define DSHOT_VERSION 150
+#endif
 
 // This ad-hoc value have to be "guessed".  It specifies how much in
 // advance we are going to clear zero bits in dshot frames. Too small
 // value makes ESC to interpret 0 bits as being 1. Too large will
 // makes ESC not to recognize the protocol.
+#ifndef DSHOT_AD_HOC_OFFSET
 #define DSHOT_AD_HOC_OFFSET     (DSHOT_T0H_ns / 5)
-
-
-
+#endif
 
 //////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
@@ -90,10 +91,46 @@ https://stackoverflow.com/questions/69425540/execute-mmap-on-linux-kernel
 
 #define NUM_PINS                27
 
-    
+enum {
+    DSHOT_CMD_MOTOR_STOP = 0,
+    DSHOT_CMD_BEACON1,
+    DSHOT_CMD_BEACON2,
+    DSHOT_CMD_BEACON3,
+    DSHOT_CMD_BEACON4,
+    DSHOT_CMD_BEACON5,
+    DSHOT_CMD_ESC_INFO, // V2 includes settings
+    DSHOT_CMD_SPIN_DIRECTION_1,
+    DSHOT_CMD_SPIN_DIRECTION_2,
+    DSHOT_CMD_3D_MODE_OFF,
+    DSHOT_CMD_3D_MODE_ON,
+    DSHOT_CMD_SETTINGS_REQUEST, // Currently not implemented
+    DSHOT_CMD_SAVE_SETTINGS,
+    DSHOT_CMD_SPIN_DIRECTION_NORMAL = 20,
+    DSHOT_CMD_SPIN_DIRECTION_REVERSED = 21,
+    DSHOT_CMD_LED0_ON, // BLHeli32 only
+    DSHOT_CMD_LED1_ON, // BLHeli32 only
+    DSHOT_CMD_LED2_ON, // BLHeli32 only
+    DSHOT_CMD_LED3_ON, // BLHeli32 only
+    DSHOT_CMD_LED0_OFF, // BLHeli32 only
+    DSHOT_CMD_LED1_OFF, // BLHeli32 only
+    DSHOT_CMD_LED2_OFF, // BLHeli32 only
+    DSHOT_CMD_LED3_OFF, // BLHeli32 only
+    DSHOT_CMD_AUDIO_STREAM_MODE_ON_OFF = 30, // KISS audio Stream mode on/Off
+    DSHOT_CMD_SILENT_MODE_ON_OFF = 31, // KISS silent Mode on/Off
+    DSHOT_CMD_SIGNAL_LINE_TELEMETRY_DISABLE = 32,
+    DSHOT_CMD_SIGNAL_LINE_CONTINUOUS_ERPM_TELEMETRY = 33,
+    DSHOT_CMD_MAX = 47
+};
+
 // I/O access
 static void                     *dshotGpioMap;
 static volatile uint32_t        *dshotGpio;
+
+// 3D mode, if dshot3dMode != 0 then reverse rotation is enabled
+static int dshot3dMode = 0;
+
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 
 static inline uint64_t dshotGetNanoseconds() {
@@ -123,7 +160,7 @@ static int dshotAddChecksumAndTelemetry(int packet, int telem) {
     return ((packet_telemetry << 4) | csum);
 }
 
-static void dshotSendFrames(uint32_t allMotorsPinMask, uint32_t *clearMasks) {
+static void dshotSend(uint32_t allMotorsPinMask, uint32_t *clearMasks) {
     int                 i, j;
     int64_t             t, tt, t0, offset1, offset2, offset3;
     volatile unsigned   *gpioset;
@@ -171,6 +208,39 @@ static void dshotSendFrames(uint32_t allMotorsPinMask, uint32_t *clearMasks) {
         }
     }
     printf("debug Dshot Frame failure.\n"); fflush(stdout);
+}
+
+static uint32_t dshotGetAllMotorsPinMask(int motorPins[], int motorMax) {
+    int         i;
+    uint32_t    allMotorsPinsMask;
+
+    // compute masks
+    allMotorsPinsMask = 0;
+    for(i=0; i<motorMax; i++) allMotorsPinsMask |= (1<<motorPins[i]);
+    return(allMotorsPinsMask);
+}
+
+void dshotSendFrames(int motorPins[], int motorMax, unsigned frame[]) {
+    int         i, bi;
+    unsigned    bit;
+    uint32_t    clearMasks[16];
+    uint32_t    msk, allMotorsMask;
+
+    assert(motorMax < NUM_PINS);
+
+    allMotorsMask = dshotGetAllMotorsPinMask(motorPins, motorMax);
+
+    // compute masks for zero bits in all frames
+    for(bi=0; bi<16; bi++) {
+        msk = 0;
+        bit = (0x8000 >> bi);
+        for(i=0; i<motorMax; i++) {
+            if ((frame[i] & bit) == 0) msk |= (1<<motorPins[i]);
+        }
+        clearMasks[bi] = msk;
+    }
+
+    dshotSend(allMotorsMask, clearMasks);
 }
 
 static uint32_t getGpioRegBase(void) {
@@ -239,70 +309,94 @@ static void dshotSetupIo() {
 
 }
 
-
-static uint32_t dshotGetAllMotorsPinMask(int motorPins[], int motorMax) {
+// Send a command repeatedly during a given perion of time
+static void dshotRepeatSendCommand(int motorPins[], int motorMax, int cmd, int telemetry, int timePeriodMsec) {
+    unsigned    frame[NUM_PINS+1];
     int         i;
-    uint32_t    allMotorsPinsMask;
-
-    // compute masks
-    allMotorsPinsMask = 0;
-    for(i=0; i<motorMax; i++) allMotorsPinsMask |= (1<<motorPins[i]);
-    return(allMotorsPinsMask);
+    int64_t     t;
+    
+    for(i=0; i<motorMax; i++) frame[i] = dshotAddChecksumAndTelemetry(cmd, telemetry);
+    t = dshotGetNanoseconds() + timePeriodMsec * 1000000LL;
+    while (dshotGetNanoseconds() <= t) {
+        dshotSendFrames(motorPins, motorMax, frame);
+        usleep(USLEEP_BEFORE_REBROADCAST);
+    }
 }
-
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Main exported functions of the module implementing raspilot motor instance.
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 void motorImplementationInitialize(int motorPins[], int motorMax) {
-    int i, pin;
+    int         i, pin;
     
     dshotSetupIo();
     
     for(i=0; i<motorMax; i++) {
         pin = motorPins[i];
-        INP_GPIO(pin); // must use INP_GPIO before we can use OUT_GPIO
+        INP_GPIO(pin);          // must use INP_GPIO before we can use OUT_GPIO
         OUT_GPIO(pin);
         GPIO_CLR = 1<<pin;
     }
+
+    // send zero throttle for 5 seconds to initialize ESCs
+    dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_MOTOR_STOP, 0, 5000);
 }
 
 void motorImplementationFinalize(int motorPins[], int motorMax) {
     munmap(dshotGpioMap, BLOCK_SIZE);
 }
 
+// This function allows to set bidirectional rotation (mode3dFlag!=0) and reverse rotation logic (reverseDirectionFlag!=0).
+// Changing 3D mode is interfering with rotation direction (at least on my ESC), so always reset the direction when changing 3D.
+void motorImplementationSet3dModeAndSpinDirection(int motorPins[], int motorMax, int mode3dFlag, int reverseDirectionFlag) {
+    int         repeatMsec;
+
+    // Repeat the command for some time to take effect. Do it longer in our case as we are not sure to send 
+    // all the frames correctly. If repeatMsec == 20, then this function will take 4*20 msec.
+    repeatMsec = 20;
+    
+    dshot3dMode = mode3dFlag;
+    if (dshot3dMode) {
+        dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_3D_MODE_ON, 1, repeatMsec);
+    } else {
+        dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_3D_MODE_OFF, 1, repeatMsec);
+    }
+    dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_SAVE_SETTINGS, 0, repeatMsec);
+
+    if (reverseDirectionFlag) {
+        dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_SPIN_DIRECTION_REVERSED, 1, repeatMsec);
+    } else {
+        dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_SPIN_DIRECTION_NORMAL, 1, repeatMsec);
+    }
+    dshotRepeatSendCommand(motorPins, motorMax, DSHOT_CMD_SAVE_SETTINGS, 0, repeatMsec);
+}
+
 void motorImplementationSendThrottles(int motorPins[], int motorMax, double motorThrottle[]) {
-    int         i, bi;
+    int         i;
     unsigned    frame[NUM_PINS+1];
-    unsigned    val, bit;
-    uint32_t    msk, allMotorsMask;
-    uint32_t    clearMasks[16];
+    int         val;
 
     assert(motorMax < NUM_PINS);
 
-    allMotorsMask = dshotGetAllMotorsPinMask(motorPins, motorMax);
-
-    // translate double throttles ranging <0, 1> to dshot frames.
     for(i=0; i<motorMax; i++) {
-        val = motorThrottle[i] * 1999 + 48;
+        if (dshot3dMode) {
+            // translate double throttles ranging <-1, 1> to dshot frames.
+            if (motorThrottle[i] > 0) {
+                val = motorThrottle[i] * 998 + 1049;
+            } else {
+                val = -motorThrottle[i] * 999 + 48;
+            }
+        } else {
+            // translate double throttles ranging <0, 1> to dshot frames.
+            val = motorThrottle[i] * 1999 + 48;
+        }
         // we use command 0 for zero thrust which should be used as arming sequence as well.
-        if (motorThrottle[i] == 0) val = 0;
+        if (motorThrottle[i] == 0 || val < 48 || val >= 2048) val = DSHOT_CMD_MOTOR_STOP;
         frame[i] = dshotAddChecksumAndTelemetry(val, 0);
     }
 
-    // compute masks for zero bits in all frames
-    for(bi=0; bi<16; bi++) {
-        msk = 0;
-        bit = (0x8000 >> bi);
-        for(i=0; i<motorMax; i++) {
-            if ((frame[i] & bit) == 0) msk |= (1<<motorPins[i]);
-        }
-        clearMasks[bi] = msk;
-    }
-
-    dshotSendFrames(allMotorsMask, clearMasks);
+    dshotSendFrames(motorPins, motorMax, frame);
 }
 
 
--- test2.c
+++ test2.c
@@ -1,13 +1,17 @@
+/*
+  This example rotates 4 motors, each of them in normal and then reversed direction.
+ */
 #include <stdio.h>
 #include <unistd.h>
 
 extern void motorImplementationInitialize(int motorPins[], int motorMax) ;
 extern void motorImplementationFinalize(int motorPins[], int motorMax) ;
 extern void motorImplementationSendThrottles(int motorPins[], int motorMax, double motorThrottle[]) ;
-
+extern void motorImplementationSet3dModeAndSpinDirection(int motorPins[], int motorMax, int mode3dFlag, int reverseDirectionFlag) ;
 
 // let's say we have 4 motors connected to GPIO6, GPIO13, GPIO19 and GPIO26
 #define N 4
+//int motorPins[N] = {6, 13, 19, 26};
 int motorPins[N] = {6, 13, 19, 26};
 
 double throttles[N];
@@ -16,18 +20,31 @@ int main() {
     int i, k;
 
     motorImplementationInitialize(motorPins, N);
-
-    // send 0 throttle during 5 seconds
-    for(i=0; i<N; i++) throttles[i] = 0;
-    for(i=0; i<500; i++) {
-        motorImplementationSendThrottles(motorPins, N, throttles);
-        usleep(10000);
-    }
+    motorImplementationSet3dModeAndSpinDirection(motorPins, N, 1, 0);
 
     // make spinning 1 motor after another
+    for(i=0; i<N; i++) throttles[i] = 0;
     for(k=0;k<12;k++) {
-	for(i=0; i<N; i++) throttles[i] = 0;
-	throttles[k%4] = 0.15;
+	// spin
+	throttles[k%4] = 0.02;
+	for(i=0; i<100; i++) {
+	    motorImplementationSendThrottles(motorPins, N, throttles);
+	    usleep(10000);
+	}
+	// stop
+	throttles[k%4] = 0;
+	for(i=0; i<100; i++) {
+	    motorImplementationSendThrottles(motorPins, N, throttles);
+	    usleep(10000);
+	}
+	// inverse spin
+	throttles[k%4] = -0.02;
+	for(i=0; i<100; i++) {
+	    motorImplementationSendThrottles(motorPins, N, throttles);
+	    usleep(10000);
+	}
+	// stop
+	throttles[k%4] = 0;
 	for(i=0; i<100; i++) {
 	    motorImplementationSendThrottles(motorPins, N, throttles);
 	    usleep(10000);
